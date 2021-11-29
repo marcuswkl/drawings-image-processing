330b97b3ec867b3d28dd4ef7d3c218ebb7e78d1f
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2021-11-29 22:19:06 +0800

    Simplify and rename program to main.py

--- main.py
+++ main.py
@@ -6,16 +6,9 @@
 # 3. Seow Yee Ying 18047449
 # 4. Thum Yong Jie 19023233
 
-import numpy as np
-import cv2
-from matplotlib import pyplot as pt
-import pytesseract as ptsr
-import openpyxl as px
 import requirement1
 import requirement2
 import requirement3
-
-ptsr.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 # Requirement 1
 # Able to identify and extract the drawing number (e.g. SU BOL E 01 27 09 10 B) from the set of engineering drawings given to you and save them into an Excel file.
@@ -23,11 +16,11 @@ requirement1.execute()
 
 # Requirement 2
 # Able to also extract other information (e.g. drawing title, author, status, etc.) from the title block and save them into an Excel file. The contents should match with the respective field titles extracted from the engineering drawing.
-requirement2.execute()
+# requirement2.execute()
 
 # Requirement 3
 # Able to identify and extract the drawing (only the drawing) and save it into a separate image file with .png extension.
-requirement3.execute()
+# requirement3.execute()
 
 # Requirement 4
 # Able to handle an additional engineering drawing with layout design not known to you (will not be “very different” from the engineering drawings given to you).
