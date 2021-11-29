85a929fc9b9bf415c6e208029d4ac2e85b5a5e90
Marcus Wong Ke Lun <sucramgnow0110@gmail.com>
2021-11-29 22:56:49 +0800

    Revert "Update requirement3"
    This reverts commit 8f094d9fa7cba2cc5c198239761ee10c65c1fa7a.

--- requirement3.py
+++ requirement3.py
@@ -1,135 +1,128 @@
-# CSC2014: Digital Imaging Processing
-# Coursework / Assignment 2021
-# Group Members:
-# 1. Marcus Wong Ke Lun 18126672
-# 2. Ko Jia Xin 17102674
-# 3. Seow Yee Ying 18047449
-# 4. Thum Yong Jie 19023233
+def execute():
+    print("Requirement 3 Code")
+
+#--------------------extract drawing 01, 02, 03--------------------
 
 import pytesseract as tess
 import cv2
 import numpy as np
 
-tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'   
+tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-def execute():
+img_ori = cv2.imread("01.png", cv2.IMREAD_GRAYSCALE)
+# img_ori = cv2.imread("02.png", cv2.IMREAD_GRAYSCALE)
+# img_ori = cv2.imread("03.png", cv2.IMREAD_GRAYSCALE)
+
+thresh = 128
+img = cv2.threshold(img_ori, thresh, 255, cv2.THRESH_BINARY)[1]
+
+hImg,wImg = img.shape
+
+for columns in range(5,wImg):
+    number_of_pixel = 0
+    table_edge = 0
+    for rows in range(5,hImg):
+        if img[rows, columns] == 0:
+            number_of_pixel = number_of_pixel + 1
+    if number_of_pixel >= (0.75*hImg):
+        table_edge = columns
+        break
     
-    #--------------------extract drawing 01, 02, 03--------------------
+drawing = img[4:3149,4:table_edge]
+drawing_resize = cv2.resize(drawing, (328, 629))
+
+cv2.imshow("Only Drawing (Extracted)", drawing_resize)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite("drawing_extracted.png", drawing)
+
+#--------------------extract drawing 04, 05--------------------
+import pytesseract as tess
+import cv2
+import numpy as np
+
+tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+img_ori = cv2.imread("04.png", cv2.IMREAD_GRAYSCALE)
+# img_ori = cv2.imread("05.png", cv2.IMREAD_GRAYSCALE)
+
+thresh = 128
+img = cv2.threshold(img_ori, thresh, 255, cv2.THRESH_BINARY)[1]
+
+hImg,wImg = img.shape
     
-    img_ori = cv2.imread("01.png", cv2.IMREAD_GRAYSCALE)
-    # img_ori = cv2.imread("02.png", cv2.IMREAD_GRAYSCALE)
-    # img_ori = cv2.imread("03.png", cv2.IMREAD_GRAYSCALE)
+for rows in range(5, hImg):
+    number_of_pixel = 0
+    table_top = 0
+    for columns in range(5, wImg):
+        if img[rows, columns] == 0:
+            number_of_pixel = number_of_pixel + 1
+    if number_of_pixel >= (0.75*hImg):
+        table_top = rows
+        break
     
-    thresh = 128
-    img = cv2.threshold(img_ori, thresh, 255, cv2.THRESH_BINARY)[1]
+drawing = img[5:table_top, 5:wImg]
+drawing_resize = cv2.resize(drawing, (629, 389))
+
+cv2.imshow("Only Drawing (Extracted)", drawing_resize)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite("drawing_extracted.png", drawing)
+
+#--------------------extract drawing 06, 07--------------------
+
+import pytesseract as tess
+import cv2
+import numpy as np
+
+tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+img_ori = cv2.imread("06.png", cv2.IMREAD_GRAYSCALE)
+# img_ori = cv2.imread("07.png", cv2.IMREAD_GRAYSCALE)
+
+thresh = 128
+img = cv2.threshold(img_ori, thresh, 255, cv2.THRESH_BINARY)[1]
+
+hImg,wImg = img.shape
+
+for columns in range(5,wImg):
+    number_of_pixel = 0
+    table_edge = 0
+    for rows in range(5,hImg):
+        if img[rows, columns] == 0:
+            number_of_pixel = number_of_pixel + 1
+    if number_of_pixel >= (0.75*hImg):
+        table_edge = columns
+        break
     
-    hImg,wImg = img.shape
-    
-    for columns in range(5,wImg):
-        number_of_pixel = 0
-        table_edge = 0
-        for rows in range(5,hImg):
-            if img[rows, columns] == 0:
-                number_of_pixel = number_of_pixel + 1
-        if number_of_pixel >= (0.75*hImg):
-            table_edge = columns
-            break
-        
-    drawing = img[4:3149,4:table_edge]
-    drawing_resize = cv2.resize(drawing, (328, 629))
-    
-    cv2.imshow("Only Drawing (Extracted)", drawing_resize)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
-    cv2.imwrite("drawing_extracted.png", drawing)
-    
-    #--------------------extract drawing 04, 05--------------------
-    import pytesseract as tess
-    import cv2
-    import numpy as np
-    
-    tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-    
-    img_ori = cv2.imread("04.png", cv2.IMREAD_GRAYSCALE)
-    # img_ori = cv2.imread("05.png", cv2.IMREAD_GRAYSCALE)
-    
-    thresh = 128
-    img = cv2.threshold(img_ori, thresh, 255, cv2.THRESH_BINARY)[1]
-    
-    hImg,wImg = img.shape
-        
-    for rows in range(5, hImg):
-        number_of_pixel = 0
-        table_top = 0
-        for columns in range(5, wImg):
-            if img[rows, columns] == 0:
-                number_of_pixel = number_of_pixel + 1
-        if number_of_pixel >= (0.75*hImg):
-            table_top = rows
-            break
-        
-    drawing = img[5:table_top, 5:wImg]
-    drawing_resize = cv2.resize(drawing, (629, 389))
-    
-    cv2.imshow("Only Drawing (Extracted)", drawing_resize)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
-    cv2.imwrite("drawing_extracted.png", drawing)
-    
-    #--------------------extract drawing 06, 07--------------------
-    
-    import pytesseract as tess
-    import cv2
-    import numpy as np
-    
-    tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-    
-    img_ori = cv2.imread("06.png", cv2.IMREAD_GRAYSCALE)
-    # img_ori = cv2.imread("07.png", cv2.IMREAD_GRAYSCALE)
-    
-    thresh = 128
-    img = cv2.threshold(img_ori, thresh, 255, cv2.THRESH_BINARY)[1]
-    
-    hImg,wImg = img.shape
-    
-    for columns in range(5,wImg):
-        number_of_pixel = 0
-        table_edge = 0
-        for rows in range(5,hImg):
-            if img[rows, columns] == 0:
-                number_of_pixel = number_of_pixel + 1
-        if number_of_pixel >= (0.75*hImg):
-            table_edge = columns
-            break
-        
-    drawing = img[4:3149,4:2370]
-    drawing_resize = cv2.resize(drawing, (680, 798))
-    
-    cv2.imshow("Only Drawing (Extracted)", drawing_resize)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
-    cv2.imwrite("drawing_extracted.png", drawing)
-    
-    #--------------------extract drawing 20--------------------
-    
-    import cv2
-    import pytesseract
-    import numpy as np
-    import matplotlib.pyplot as plt
-    
-    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
-    
-    img = cv2.imread("20.png", 0)
-    
-    img [1547:2050, 180:1000] = 255
-    final = img[258:, 0:2363]
-    
-    cv2.imshow("Drawing 20", final)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-    
-    cv2.imwrite("drawing20.png", final)
-    #test
+drawing = img[4:3149,4:2370]
+drawing_resize = cv2.resize(drawing, (680, 798))
+
+cv2.imshow("Only Drawing (Extracted)", drawing_resize)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite("drawing_extracted.png", drawing)
+
+#--------------------extract drawing 20--------------------
+
+import cv2
+import pytesseract
+import numpy as np
+import matplotlib.pyplot as plt
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+
+img = cv2.imread("20.png", 0)
+
+img [1547:2050, 180:1000] = 255
+final = img[258:, 0:2363]
+
+cv2.imshow("Drawing 20", final)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+cv2.imwrite("drawing20.png", final)
+#test
